fbe224cfdb1f6552e9b9e9d5e8730a276d36011e
Kevin K H Zheng <kevin.k.h.zheng@qq.com>
2020-12-19 16:23:18 +0800

    tree shaking 打包时去除无用代码

---
+++ 22.tree shaking/src/js/index.js
@@ -0,0 +1,27 @@
+// 引入 iconfont 样式文件
+import '../styles/iconfont.css';
+import '../styles/index.less';
+import { print } from './print';
+
+/**
+ * vscode 需要配置以下内容才能与prettierrc配置格式化
+ {
+  // eslint配置项，保存时自动修复
+  "editor.codeActionsOnSave": {
+    "source.fixAll.eslint": true
+  },
+  // 默认使用prettier格式化支持的文件
+  "editor.defaultFormatter": "esbenp.prettier-vscode",
+  // 自动设定eslint工作区
+  "eslint.workingDirectories": [{ "mode": "auto" }],
+  "editor.formatOnSave": true
+ }
+ */
+
+const log = () => {
+  // eslint-disable-next-line no-console
+  console.log('Hello world');
+};
+
+log();
+print();
