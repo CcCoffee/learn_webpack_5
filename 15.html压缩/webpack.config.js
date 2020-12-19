bd73e42794bb8ee95c3384813aa74754add6dddb
Kevin K H Zheng <kevin.k.h.zheng@qq.com>
2020-12-19 10:47:23 +0800

    生产环境配置

---
+++ 15.html压缩/webpack.config.js
@@ -0,0 +1,64 @@
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'js/built.js',
+    path: resolve(__dirname, 'build'),
+  },
+  target: ['web', 'es5'], // 兼容IE必须加上 target
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          // 预设，指示 babel 如何进行兼容性处理
+          presets: [
+            ['@babel/preset-env', {
+              // 按需加载兼容性代码
+              useBuiltIns: 'usage',
+              // 指定corejs版本
+              corejs: {
+                version: 3,
+              },
+              // 指定兼容性做到哪个版本的浏览器
+              targets: {
+                ie: '9',
+                chrome: '60',
+                firefox: '50',
+                safari: '10',
+                edge: '17',
+              },
+            }],
+          ],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: {
+        // 移除空格
+        collapseWhitespace: true,
+        // 移除注释
+        removeComments: true,
+      },
+    }),
+  ],
+  // 生产环境下本来就会自动压缩html
+  mode: 'production',
+  devServer: {
+    // 构建后的路径
+    contentBase: resolve(__dirname, 'build'),
+    // 启动gzip压缩
+    compress: true,
+    open: false,
+    // 自动打开默认浏览器
+    port: 3000,
+  },
+  devtool: 'inline-source-map',
+};
---
+++ 16.生产环境配置/src/js/index.js
@@ -0,0 +1,10 @@
+// 引入 iconfont 样式文件
+import '../styles/iconfont.css';
+import '../styles/index.less';
+
+const log = () => {
+  // eslint-disable-next-line no-console
+  console.log('Hello');
+};
+
+log();
